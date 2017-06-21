df4ddb06082bd89dbaa855767f5ce0dbef7c8375
asthajain23 <asthajain2308@gmail.com>
2017-06-21 12:12:36 +0530

    Update puppet4.pp

--- puppet4.pp
+++ puppet4.pp
@@ -1,7 +1,22 @@
 #Write a script which will configure a git and clone some repository through it.
 
-exec { 'git': 
+class git { 
+exec { 'git':
         command => 'sudo yum install git -y',
         path => '/usr/bin/',
-       
+}
+
+include git
+git::config{'user.name':
+   value => 'asthajain23'
+}
+git::config{'user.email':
+ value => 'asthajain2308@gmail.com'
+
+
+}exec { 'git_clone':
+        command => 'git clone https://github.com/asthajain23/puppetque.git -y',
+        path => '/usr/bin/',
+}
+
 }
