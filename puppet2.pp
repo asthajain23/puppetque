994a489d24b6bb2d570862a5bb74655943c4d74b
asthajain23 <asthajain2308@gmail.com>
2017-06-27 17:45:56 +0530

    Update puppet2.pp

--- puppet2.pp
+++ puppet2.pp
@@ -7,3 +7,11 @@ user { 'puppetuserastha':
 groups => ['puppetasthagroup'],
     managehome => 'true',
   }
+
+'''Output
+[root@demo1 manifests]# puppet apply puppet2.pp
+Notice: Compiled catalog for demo1.c.skilful-tiger-169411.internal in environment production in 0.20 seconds
+Notice: Finished catalog run in 0.02 seconds 
+[root@demo1 manifests]# cat /etc/group
+
+puppetasthagroup:x:1006:puppetuserastha   '''
